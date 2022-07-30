be5d5ea9a61b4baf789222e01268a016459c2c92
ishfaq ahmad gojree <90743868+Ishfaq10@users.noreply.github.com>
2022-07-30 14:08:30 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+package com.ishfaqahmad04;
+//Write a program to calculate CGPA using marks of three subjects (out of 100)
+
+public class First {
+    public static void main(String[] args) {
+        float subject1 = 45;
+        float subject2 = 95;
+        float subject3 = 48;
+        float cgpa = (subject1 + subject2 + subject3)/30;
+        System.out.println(cgpa);
+    }
+}
